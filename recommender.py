5f24a42a975801222d95f73fe917802638b41201
Amritanshu Mishra <amritanshu99@gmail.com>
2025-07-08 16:03:24 +0530

    Compressed

--- recommender.py
+++ recommender.py
@@ -1,21 +1,24 @@
 # recommender.py
-import joblib
+import pickle
 from rapidfuzz import process
 
-print("📦 Loading recommender model (joblib)...")
+print("📦 Loading recommender model (pickle)...")
 try:
-    df, cosine_sim, indices = joblib.load("recommender_compressed.pkl")
+    with open("recommender.pkl", "rb") as f:
+        df, cosine_sim, indices = pickle.load(f)
     print("✅ Model loaded successfully!")
 except Exception as e:
     print("❌ Failed to load model:", e)
     raise
 
+# Match closest movie title using fuzzy match
 def get_closest_title(input_title, all_titles):
     match = process.extractOne(input_title, all_titles, score_cutoff=60)
     if match:
         return match[0]
     return None
 
+# Recommend similar movies
 def recommend_movies(input_title, num=5):
     actual_title = get_closest_title(input_title, df["title"])
     if not actual_title:
